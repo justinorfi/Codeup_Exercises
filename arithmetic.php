710b5798d3f764a01edc4e999389e395cc01e2fa
Justin Orfi <kornyrock317@gmail.com>
2015-06-15 19:52:37 -0500

    Spread out data to be easily read

--- arithmetic.php
+++ arithmetic.php
@@ -24,8 +24,13 @@ function modulus($a, $b)
 	return $a % $b;
 }
 echo add(5, 4);
+echo PHP_EOL;
 echo subtract(9, 1);
+echo PHP_EOL;
 echo multiply(5, 8);
+echo PHP_EOL;
 echo divide(12, 4);
+echo PHP_EOL;
 echo modulus(100, 20);
+echo PHP_EOL;
 ?>
